0bde3c3151f8198c473862360d91dc885ea877cb
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-07-29 22:28:37 +0900

    Set up backend project Install needed modules.
    Set config files of lint, jest, babel.
    
    Apply express for route.

---
+++ backend/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  setupFilesAfterEnv: [
+    'given2/setup',
+    'jest-plugin-context/setup',
+    './jest.setup',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+};
