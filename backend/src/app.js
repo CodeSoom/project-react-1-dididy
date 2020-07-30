5babf3a0b07daf60ad752ca3037a9e71ae6ddc4f
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-07-31 02:16:38 +0900

    Apply simple-peer with socket.io Configure backend using socket.io.
    Configure frontend using simple-peer.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
-import express from 'express';
+const express = require('express');
+const http = require('http');
 
 const app = express();
 
@@ -6,4 +7,33 @@ app.get('/', (req, res) => {
   res.status(200).send({ message: 'Hello World!' });
 });
 
-export default app;
+const server = http.createServer(app);
+const socket = require('socket.io');
+
+const io = socket(server);
+
+const users = {};
+
+io.on('connection', (requestSocket) => {
+  if (!users[requestSocket.id]) {
+    users[requestSocket.id] = requestSocket.id;
+  }
+  requestSocket.emit('yourID', requestSocket.id);
+  io.sockets.emit('allUsers', users);
+  requestSocket.on('disconnect', () => {
+    delete users[requestSocket.id];
+  });
+
+  requestSocket.on('callUser', (data) => {
+    io.to(data.userToCall).emit('hey', {
+      signal: data.signalData,
+      from: data.from,
+    });
+  });
+
+  requestSocket.on('acceptCall', (data) => {
+    io.to(data.to).emit('callAccepted', data.signal);
+  });
+});
+
+server.listen(8000, () => console.log('server is running on port 8000'));
--- frontend/src/components/MainSidebar.jsx
+++ frontend/src/components/MainSidebar.jsx
@@ -1,11 +1,161 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
-import { MainSidebarWrapper } from './MainSidebar.style';
+import io from 'socket.io-client';
+import Peer from 'simple-peer';
+
+import { MainSidebarWrapper, ButtonWrapper, WebcamContainerWrapper } from './MainSidebar.style';
 
 export default function MainSidebar() {
+  const [yourID, setYourID] = useState('');
+  const [users, setUsers] = useState({});
+  const [stream, setStream] = useState();
+  const [receivingCall, setReceivingCall] = useState(false);
+  const [caller, setCaller] = useState('');
+  const [callerSignal, setCallerSignal] = useState();
+  const [callAccepted, setCallAccepted] = useState(false);
+
+  const userVideo = useRef();
+  const partnerVideo = useRef();
+  const socket = useRef();
+
+  useEffect(() => {
+    socket.current = io.connect('/');
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((requestStream) => {
+        setStream(requestStream);
+        if (userVideo.current) {
+          userVideo.current.srcObject = requestStream;
+        }
+      });
+
+    socket.current.on('yourID', (id) => {
+      setYourID(id);
+    });
+    socket.current.on('allUsers', (requestUsers) => {
+      setUsers(requestUsers);
+    });
+
+    socket.current.on('hey', (data) => {
+      setReceivingCall(true);
+      setCaller(data.from);
+      setCallerSignal(data.signal);
+    });
+  }, []);
+
+  function callPeer(id) {
+    setCaller(id);
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      config: {
+        iceServers: [
+          { url: 'stun:stun.l.google.com:19302' },
+          {
+            urls: 'stun:numb.viagenie.ca',
+            username: 'sultan1640@gmail.com',
+            credential: '98376683',
+          },
+          {
+            url: 'turn:numb.viagenie.ca',
+            credential: 'muazkh',
+            username: 'webrtc@live.com',
+          },
+        ],
+      },
+      stream,
+    });
+
+    peer.on('signal', (data) => {
+      socket.current.emit('callUser', {
+        userToCall: id,
+        signalData: data,
+        from: yourID,
+      });
+    });
+
+    peer.on('stream', (requestStream) => {
+      if (partnerVideo.current) {
+        partnerVideo.current.srcObject = requestStream;
+      }
+    });
+
+    socket.current.on('callAccepted', (signal) => {
+      setCallAccepted(true);
+      peer.signal(signal);
+    });
+  }
+
+  function acceptCall() {
+    setCallAccepted(true);
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream,
+    });
+    peer.on('signal', (data) => {
+      socket.current.emit('acceptCall', { signal: data, to: caller });
+    });
+
+    peer.on('stream', (requestStream) => {
+      partnerVideo.current.srcObject = requestStream;
+    });
+
+    peer.signal(callerSignal);
+  }
+
+  const IncomingCall = (
+    <div>
+      <h1>
+        {caller}
+        is calling you
+      </h1>
+      <button type="button" onClick={acceptCall}>
+        Accept
+      </button>
+    </div>
+  );
+
+  const ConnectedIndicator = (
+    <div>
+      Connected with
+      {' '}
+      {caller}
+    </div>
+  );
+
+  const renderByCallStatus = (receivingCall ? IncomingCall : '');
+
+  const UserVideo = <video playsInline muted ref={userVideo} autoPlay />;
+
+  const PartnerVideo = <video playsInline muted ref={partnerVideo} autoPlay />;
+
   return (
     <MainSidebarWrapper>
-      MainSidebar
+      <div>
+        MainSidebar
+        <br />
+        My id:
+        {' '}
+        {yourID}
+      </div>
+      {Object.keys(users).map((key) => {
+        if (key === yourID) {
+          return null;
+        }
+        return (
+          <ButtonWrapper key={key} type="button" onClick={() => callPeer(key)} toggle={callAccepted}>
+            Call to
+            {' '}
+            {key}
+          </ButtonWrapper>
+        );
+      })}
+      {callAccepted ? ConnectedIndicator : renderByCallStatus}
+      <WebcamContainerWrapper>
+        {stream ? UserVideo : ''}
+        {callAccepted ? PartnerVideo : ''}
+      </WebcamContainerWrapper>
     </MainSidebarWrapper>
   );
 }
